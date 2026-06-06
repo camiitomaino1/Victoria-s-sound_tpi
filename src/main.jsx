e93f79ebfcf2e493f74c5a89ffa76e20156243c5
Camila <camii.tomaino@gmail.com>
2026-06-06 16:52:12 -0300

    main.jsx with CartProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { CartProvider } from './context/CartContext'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </StrictMode>
 )
