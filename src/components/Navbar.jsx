5c1d85db77ea3d6ea28d321953162e2ea5995f37
Camila <camii.tomaino@gmail.com>
2026-06-06 18:17:42 -0300

    Navbar.jsx with CartContext

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { Navbar as BsNavbar, Nav, Container } from 'react-bootstrap'
+import { Navbar as BsNavbar, Nav, Container, Badge } from 'react-bootstrap'
+import { CartContext } from '../context/CartContext'
 
 const Navbar = () => {
+
+  // Connect to CartContext to read the cart state
+  const { cart } = useContext(CartContext)
+
+  // Calculate total number of items across all products
+  const totalItems = cart.reduce((total, item) => total + item.quantity, 0)
+
   return (
     <BsNavbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <BsNavbar.Brand as={Link} to="/">Victoria´s Sound</BsNavbar.Brand>
+        <BsNavbar.Brand as={Link} to="/">🎸 Victoria's Sound</BsNavbar.Brand>
         <BsNavbar.Toggle aria-controls="main-nav" />
         <BsNavbar.Collapse id="main-nav">
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/">Inicio</Nav.Link>
             <Nav.Link as={Link} to="/products">Productos</Nav.Link>
+
+            {/* Cart link with item counter badge */}
+            <Nav.Link as={Link} to="/cart">
+              Carrito{' '}
+              {totalItems > 0 && (
+                <Badge bg="danger" pill>
+                  {totalItems}
+                </Badge>
+              )}
+            </Nav.Link>
+
             <Nav.Link as={Link} to="/login">Iniciar sesión</Nav.Link>
             <Nav.Link as={Link} to="/register">Registrarse</Nav.Link>
           </Nav>
